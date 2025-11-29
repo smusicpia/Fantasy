fed924b962fa8bdc98c1a238750b5865bdf80241
Servando Hernandez <s_musicpia@yahoo.com.mx>
2025-11-28 18:01:09 -0600

    Add a First Controller

--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -27,4 +27,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.Run();
