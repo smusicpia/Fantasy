fc3a143ef6f1283598a66fac538c3611c3ab2c0b
Servando Hernandez <s_musicpia@yahoo.com.mx>
2025-11-29 10:06:07 -0600

    Add conection between Backend and Frontend First CRUD

---
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,19 @@
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entities;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Frontend.Pages.Countries;
+
+public partial class CountriesIndex
+{
+    [Inject] private IRepository Repository { get; set; } = null!;
+
+    private List<Country>? Countries { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+        Countries = responseHppt.Response!;
+    }
+}
--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -1,4 +1,5 @@
 using Fantasy.Frontend;
+using Fantasy.Frontend.Repositories;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7158") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
